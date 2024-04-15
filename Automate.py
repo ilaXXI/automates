28ae4af17a4e3e5bf2d67876d6a530a4ac2d2223
GL150hcknt <48103294+GL150hcknt@users.noreply.github.com>
2024-04-15 16:15:10 +0200

    creation classe automate

---
+++ Automate.py
@@ -0,0 +1,7 @@
+class Automate:
+    def __init__(self, alphabet, initiaux, terminaux, transitions):
+        self.alphabet = alphabet
+        self.initiaux = initiaux
+        self.terminaux = terminaux
+        self.transitions = transitions
+
